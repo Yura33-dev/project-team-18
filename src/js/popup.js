4cda12de958c0de0fc850c95d18d1f55d05fc262
Vita1208 <111512978+Vita1208@users.noreply.github.com>
2024-06-02 09:42:06 +0300

    html +pop-up

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,37 +1,38 @@
+document.querySelector('.overlay').classList.add('is-active');
+const modalContainer = document.createElement('div');
+modalContainer.classList.add('modal-container');
 
-document.addEventListener("DOMContentLoaded", function() {
-    const modal = document.querySelector(".modal");
-    const closeButton = modal.querySelector(".icon-modal-close");
-    const openButton = document.querySelector(".button-together");
-    const form = document.querySelector(".form-message");
+const closeButton = document.createElement('button');
+closeButton.setAttribute('type', 'button');
+closeButton.classList.add('icon-modal-close');
+closeButton.innerHTML = `
+    <svg width="24" height="24">
+        <use href="./img/icons/sprite.svg#icon-close"></use>
+    </svg>
+`;
 
-    function openModal() {
-        modal.classList.add("active");
-        document.body.style.overflow = "hidden";
+const modalText = document.createElement('p');
+modalText.classList.add('modal-text');
+modalText.innerHTML = `
+    <span class="span-modal">Thank you for your interest in cooperation!<br></span> The manager will contact you shortly to discuss
+    further <br> details and opportunities for<br>cooperation. Please stay in touch.
+`;
+
+modalContainer.appendChild(closeButton);
+modalContainer.appendChild(modalText);
+
+document.querySelector('.overlay').appendChild(modalContainer);
+
+document.querySelector('.overlay').addEventListener('click', function(event) {
+    if (event.target === document.querySelector('.overlay')) {
+        document.querySelector('.overlay').classList.remove('is-active');
+        document.querySelector('.modal-container').remove();
     }
+});
 
-    function closeModal() {
-        modal.classList.remove("active");
-        document.body.style.overflow = "";
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        document.querySelector('.overlay').classList.remove('is-active');
+        document.querySelector('.modal-container').remove();
     }
-
-    closeButton.addEventListener("click", closeModal);
-
-    modal.addEventListener("click", function(event) {
-        if (event.target === modal) {
-            closeModal();
-        }
-    });
-      document.addEventListener("keydown", function(event) {
-          if (event.key === "Escape") {
-            closeModal();
-        }
-  });
-    openButton.addEventListener("click", openModal);
-
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        openModal();
-        form.reset();
-    });
 });
