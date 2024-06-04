ecce88e44836efd21c5319683d5d9a3a40a11477
Victoria Usenko <usenko.vika@gmail.com>
2024-06-04 03:01:43 +0300

    acardeon implemented. NTML and CSS rewritten

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,31 +1,13 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+import spriteSvg from '../img/icons/sprite.svg';
 
-const refs = {
-  accordionWrappers: document.querySelectorAll('.ac'),
-  acList: document.querySelector('.accordion-container'),
+const onOpenWrapp = () => {};
+
+const onClosenWrapp = () => {};
+
+const userOptions = {
+  openOnInit: [0],
+  // onOpen: onOpenWrapp(),
 };
-function clickHandler(event) {
-  const clickedElement = event.target;
-  const isAcPanelClicked = clickedElement.closest('.ac-panel');
-  if (isAcPanelClicked) {
-    return;
-  }
-  const closestAc = clickedElement.closest('.ac');
-  const activeElement = document.querySelector('.active');
-  if (activeElement && activeElement.id !== closestAc.id) {
-    activeElement.classList.remove('active');
-  }
-  closestAc.classList.toggle('is-active');
-  for (const wrapper of refs.accordionWrappers) {
-    if (wrapper === event.currentTarget) {
-      continue;
-    }
-    wrapper.classList.remove('is-active');
-  }
-  event.currentTarget.classList.toggle('is-active');
-}
-refs.accordionWrappers.forEach(wrapper => {
-  wrapper.addEventListener('click', clickHandler);
-});
-refs.acList.addEventListener('click', clickHandler);
+const accordion = new Accordion('.accordion-container', userOptions);
